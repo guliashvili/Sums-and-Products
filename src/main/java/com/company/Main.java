434afda86858af63a569a5985545517aa7a2d8d9
gguliash <gguliash@cisco.com>
2015-11-24 11:17:00 -0800

    added backup git

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -71,7 +71,7 @@ public class Main {
     public static void main(String[] args) {
 
 
-
+//
         List<SolI> pipe = new ArrayList<>();
         pipe.add(new Sol0());
         pipe.add(new Sol1());
